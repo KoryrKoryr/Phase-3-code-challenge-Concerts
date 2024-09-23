cb750f7e55650115d60486fa7139bc5ea898f35f
Denis Korir <denohrkoryr@gmail.com>
2024-09-23 10:20:08 +0300

    models defined

---
+++ models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+# Band Model
+class Band(Base):
+    __tablename__ = 'bands'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    hometown = Column(String, nullable=False)
+    
+    concerts = relationship('Concert', back_populates='band')
+
+# Venue Model
+class Venue(Base):
+    __tablename__ = 'venues'
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    
+    concerts = relationship('Concert', back_populates='venue')
+
+# Concert Model
+class Concert(Base):
+    __tablename__ = 'concerts'
+    
+    id = Column(Integer, primary_key=True)
+    date = Column(String, nullable=False)
+    band_id = Column(Integer, ForeignKey('bands.id'))
+    venue_id = Column(Integer, ForeignKey('venues.id'))
+
+    band = relationship('Band', back_populates='concerts')
+    venue = relationship('Venue', back_populates='concerts')
